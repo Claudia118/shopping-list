7aa5045ae3e8465ae8c74aebdb6939f6addcf0a0
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-08-25 11:32:06 -0600

    Add items to local storage

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const list = document.getElementById("item-list");
 const filter = document.getElementById("filter");
 const clearBtn = document.getElementById("clear");
 
+function displayItems() {
+  const addItemToStorage = getItemsFromStorage();
+
+  addItemToStorage.forEach((item) => addItemToDOM(item));
+
+  checkUI();
+}
+
 function addItem(e) {
   e.preventDefault();
 
@@ -15,9 +23,22 @@ function addItem(e) {
     return;
   }
 
+  //Creates item Dom element
+  addItemToDOM(newItemInput);
+
+  //Add item to local storage
+  addItemToStorage(newItemInput);
+
+  checkUI();
+
+  //   Clear item input
+  input.value = "";
+}
+
+function addItemToDOM(item) {
   // Create list item
   const li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItemInput));
+  li.appendChild(document.createTextNode(item));
 
   const button = createButton("remove-item btn-link");
 
@@ -25,11 +46,6 @@ function addItem(e) {
 
   //   Add to DOM
   list.appendChild(li);
-
-  checkUI();
-
-  //   Clear item input
-  input.value = "";
 }
 
 function createButton(classes) {
@@ -46,6 +62,29 @@ function createBtnIcon(classes) {
   icon.className = classes;
 
   return icon;
+}
+
+function addItemToStorage(item) {
+  const addItemToStorage = getItemsFromStorage();
+
+  //Add new item to array
+  addItemToStorage.push(item);
+
+  // Conver to JSON string and set to local storage
+  localStorage.setItem("items", JSON.stringify(addItemToStorage));
+}
+
+// Get Items from Local Storage
+function getItemsFromStorage() {
+  let addItemToStorage;
+
+  if (localStorage.getItem("items") === null) {
+    addItemToStorage = [];
+  } else {
+    addItemToStorage = JSON.parse(localStorage.getItem("items"));
+  }
+
+  return addItemToStorage;
 }
 
 // Targets the li to be removed when the icon is click
@@ -67,6 +106,21 @@ function clearItems() {
   checkUI();
 }
 
+function filterItems(e) {
+  const listItems = list.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+
+  listItems.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
 function checkUI() {
   const listItems = list.querySelectorAll("li");
 
@@ -79,9 +133,16 @@ function checkUI() {
   }
 }
 
-//Event Listeners
-form.addEventListener("submit", addItem);
-list.addEventListener("click", removeItem);
-clearBtn.addEventListener("click", clearItems);
+// Initialize app
+function init() {
+  //Event Listeners
+  form.addEventListener("submit", addItem);
+  list.addEventListener("click", removeItem);
+  clearBtn.addEventListener("click", clearItems);
+  filter.addEventListener("input", filterItems);
+  document.addEventListener("DOMContentLoaded", displayItems);
 
-checkUI();
+  checkUI();
+}
+
+init();
